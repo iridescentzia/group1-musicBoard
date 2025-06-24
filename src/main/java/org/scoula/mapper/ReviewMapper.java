86825dca042c1ef3a534658247e08ae372e7710b
iridescentzia <ziaunzip@naver.com>
2025-06-24 09:37:54 +0900

    feature/review : reviewMapper.java add

--- src/main/java/org/scoula/mapper/ReviewMapper.java
+++ src/main/java/org/scoula/mapper/ReviewMapper.java
@@ -1,4 +1,26 @@
 package org.scoula.mapper;
 
-public class ReviewMapper {
+import org.scoula.domain.ReviewVO;
+
+import java.util.List;
+
+public interface ReviewMapper {
+
+    // 전체 리뷰 목록
+    List<ReviewVO> getList();
+
+    // 특정 리뷰 조회
+    ReviewVO get(Long id);
+
+    // 리뷰 작성
+    void insert(ReviewVO review);
+
+    // 리뷰 수정
+    int update(ReviewVO review);
+
+    // 리뷰 삭제
+    int delete(Long id);
+
+    // 특정 음악의 리뷰 목록 조회
+    List<ReviewVO> getListByMusicId(Long musicId);
 }
