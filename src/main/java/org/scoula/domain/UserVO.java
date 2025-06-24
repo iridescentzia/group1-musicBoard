3f6557c70695a0e8f2dda25a1835c62079bea8d9
권혁주\rnjsg <rnjsgurwn990217@naver.com>
2025-06-24 15:43:29 +0900

    ddd

--- src/main/java/org/scoula/domain/UserVO.java
+++ src/main/java/org/scoula/domain/UserVO.java
@@ -1,4 +1,15 @@
 package org.scoula.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserVO {
+    private int id; //사용자 고유 id
+    private String username; //사용자 이름
 }
--- src/main/java/org/scoula/dto/UserDTO.java
+++ src/main/java/org/scoula/dto/UserDTO.java
@@ -1,4 +1,31 @@
 package org.scoula.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.scoula.domain.UserVO;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserDTO {
+    private int id;
+    private String username;
+
+    public static UserDTO of(UserVO vo){
+        return vo == null ? null : UserDTO.builder()
+                .id(vo.getId())
+                .username(vo.getUsername())
+                .build();
+    }
+
+    public UserVO toVO(){
+        return UserVO.builder()
+                .id(id)
+                .username(username)
+                .build();
+
+    }
 }
