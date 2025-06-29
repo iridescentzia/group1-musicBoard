622384bea63aa22411130a4e7fc0035950de4b03
iridescentzia <ziaunzip@naver.com>
2025-06-29 23:05:45 +0900

    feature/review : /api/music/1/reviews 에서 music의 releaseDate가 올바른 형식으로 출력되도록 ReviewDTO 수정

--- src/main/java/org/scoula/dto/ReviewDTO.java
+++ src/main/java/org/scoula/dto/ReviewDTO.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.scoula.domain.MusicVO;
 import org.scoula.domain.ReviewVO;
 
 import java.time.LocalDateTime;
@@ -23,6 +24,8 @@ public class ReviewDTO {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private Date createdAt;
 
+    private MusicDTO music;
+
     // VO -> DTO 변환
     public static ReviewDTO of(ReviewVO vo) {
         return vo == null ? null : ReviewDTO.builder()
@@ -32,18 +35,19 @@ public class ReviewDTO {
                 .content(vo.getContent())
                 .rating(vo.getRating())
                 .createdAt(vo.getCreatedAt())
+                .music(MusicDTO.of(vo.getMusic())) // MusicVo -> MusicDTO 변환
                 .build();
     }
 
     // DTO -> VO 변환
     public ReviewVO toVO() {
         return ReviewVO.builder()
-                .id(id)
                 .userId(userId)
                 .musicId(musicId)
                 .content(content)
                 .rating(rating)
                 .createdAt(createdAt)
+                .music(music != null ? music.toVO() : null) // MusicDTO -> MusicVO 변
                 .build();
     }
 }
