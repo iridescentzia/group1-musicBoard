897627573211f97b1f1832f1bf7541d645b83605
subeen <sju081902@gmail.com>
2025-07-07 02:05:44 +0900

    front/music : MusicList.vue 페이지 작성 완료

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import UserList from '../views/users/UserList.vue'
 
+// Music 관련 컴포넌트 import
+import MusicList from '@/views/music/MusicList.vue'
+import MusicDetail from '@/views/music/MusicDetail.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -25,6 +29,19 @@ const router = createRouter({
       name: 'UserCreate',
       component: () => import('@/views/users/UserCreate.vue')
     },
+
+    // music 라우터
+    {
+      path: '/music',
+      name: 'MusicList',
+      component: MusicList
+    },
+    {
+      path: '/music/:id',
+      name: 'MusicDetail',
+      component: MusicDetail,
+      props: true
+    }
   ],
 })
 
